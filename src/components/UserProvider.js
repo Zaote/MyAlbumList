112f33f14f5e78e8ea96dda24d9bc0b261b8d8a5
Rodrigo Soares <53272698+Zaote@users.noreply.github.com>
2023-06-21 22:29:33 -0300

    Feito mecanismo de login!

---
+++ src/components/UserProvider.js
@@ -0,0 +1,101 @@
+import React, { createContext, useReducer, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import initialData from '../initialData'
+
+const UsersContext = createContext();
+const initialState = {context: []};
+
+async function saveContext(context) {
+    try {
+        await AsyncStorage.setItem('context', JSON.stringify(context));
+    } catch (error) {
+        console.error('An error has occurred while saving the context using AsyncStorage', error);
+    }
+}
+
+async function loadContext() {
+    try {
+        const context = await AsyncStorage.getItem('context');
+        return { context: context ? JSON.parse(context) : [] };
+    } catch (error) {
+        console.log('An error has occurred while loading the users from AsyncStorage', error);
+        return { context: [] };
+    }
+}
+
+const actions = {
+    // deleteUser: (state, action) => {
+    //     const user = action.payload;
+    //     const updatedUsers = state.users.filter(u => u.id !== user.id);
+    //     saveUsers(updatedUsers);
+    //     return { ...state, users: updatedUsers };
+    // },
+    // createUser: (state, action) => {
+    //     const user = action.payload;
+    //     user.id = Math.random()
+    //     const updatedUsers = [...state.users, user];
+    //     saveUsers(updatedUsers);
+    //     return { ...state, users: updatedUsers };
+    // },
+    // updateUser: (state, action) => {
+    //     const updated = action.payload;
+    //     const updatedUsers = state.users.map(u => u.id === updated.id ? updated : u);
+    //     saveUsers(updatedUsers);
+    //     return { ...state, users: updatedUsers };
+    // },
+    loadContext: (state, action) => {
+        const loadedContext = action.payload.context;
+        return { ...state, context: loadedContext };
+    },
+    // updateMultipleUsers: (state, action) => {
+    //     const updatedUsers = state.users.map(u => {
+    //         const userUpdated = action.payload.find(usr => usr.id === u.id);
+    //         if (userUpdated) {
+    //             return userUpdated;
+    //         } else {
+    //             return u;
+    //         }
+    //     });
+    //     saveUsers(updatedUsers);
+    //     return { ...state, users: updatedUsers };
+    // },
+    // deleteAll: (state, action) => {
+    //     clearUsers()
+    //     return {...state}
+    // },
+    
+};
+
+export function UserProvider({ children }) {
+    function reducer(state, action) {
+        const fn = actions[action.type];
+        return fn ? fn(state, action) : state;
+    };
+    
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const loadedContext = await loadContext();
+
+    //         if (loadedContext.context.length !== 0) {
+    //             dispatch({ type: 'loadContext', payload: loadedContext });
+    //         } else {
+    //             dispatch({ type: 'loadContext', payload: {initialData} })
+    //             saveContext(initialData)
+    //         }
+    //     }
+
+    //     fetchData();
+    // }, []);
+
+    // const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, {context: initialData})
+    
+
+    return (
+        <UsersContext.Provider value={{state, dispatch }}>
+            {children}
+        </UsersContext.Provider>
+    )
+}
+
+export default UsersContext;
---
+++ src/initialData.js
@@ -0,0 +1,21 @@
+// Arquivo para popular o app quando não há dados salvos
+const initialData = {
+    // password: 1234567
+    Sting: {
+        givenName: "Gordon Matthew Thomas",
+        familyName: "Sumner",
+        email: 'stingthepolice@coolmail.com',
+        password: '$2a$10$Qi9sdVYBZdA./w51C8X7Y.6v7vn7ih87l256y/.hE2YcNnYNiBo3G',
+        albumData: {}
+    },
+    // password: theworld
+    Dio: {
+        givenName: "Ronald James",
+        familyName: "Padavona",
+        email: 'ronniejamesdio@coolmail.com',
+        password: '$2a$10$8lNu9MukNDP0V64CjUh5iuKH09KpHqN6g6o8bB6zcGEh6KwV.GxIK',
+        albumData: {}
+    }
+} 
+
+export default initialData;
